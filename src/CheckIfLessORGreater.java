8e7640b8ffb4cedb38de9a846f7e7f1e76b7877c
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-07-03 12:02:43 -0400

    Program to check if a number is greater or less than 10

---
+++ src/CheckIfLessORGreater.java
@@ -0,0 +1,14 @@
+public class CheckIfLessORGreater {
+
+    public static void main(String [] args){
+        int x = 7;
+
+        //Re-asign value to x, so we can see the different results
+        x = 15;
+
+        if(x < 10)
+            System.out.println("Less than 10");
+        else
+            System.out.println("Greater than 10");
+    }
+}
