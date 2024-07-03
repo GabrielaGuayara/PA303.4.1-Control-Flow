8af7437bac989d74f7193cc9daa26ed94f01e4e3
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-07-03 12:03:24 -0400

    Built a program to check a number is less than 10

---
+++ src/CheckLessThan10.java
@@ -0,0 +1,12 @@
+public class CheckLessThan10 {
+    public static void main(String[] args) {
+
+        int x = 7;
+
+        x = 15;
+
+        if(x < 10)
+            System.out.println("Less than 10");
+
+    }
+}
